e9d18a728fb3c0815b26a2589863ac831ed4b3e7
Khiet Le <khiet.le@ehochef.com>
2018-01-28 17:06:30 +0000

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user, except: [:create]
 
   def index
     render json: User.select(:id, :email), status: :ok
+  end
+
+  def create
+    user = User.new(email: params[:email], password: params[:password])
+    if user.save
+      render json: user.slice(:id, :email, :created_at), status: :created
+    else
+      render json: { error: user.errors.full_messages.join(', ') }, status: :bad_request
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
 
-  resources :users, only: [:index]
+  resources :users, only: [:index, :create]
 end

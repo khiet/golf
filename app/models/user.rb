8f3eb0b00c8133bf301c7266590236fc2041b03d
Khiet Le <khiet.le@ehochef.com>
2018-01-29 09:13:10 +0000

    add test for post users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true
+  validates :email, uniqueness: true
   validates :email, format: { with: EMAIL_REGEXP }
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,25 +3,54 @@ require 'rails_helper'
 RSpec.describe "users" do
   let(:user1) { create(:user) }
   let(:user2) { create(:user) }
-  let(:user3) { create(:user) }
 
   before do
     user1
     user2
   end
 
-  it 'returns a list of users' do
-    get '/users', headers: authorization_header(user1)
+  describe 'GET users' do
+    it 'returns a list of users' do
+      get '/users', headers: authorization_header(user1)
 
-    expect(response).to be_ok
-    expect(json_body.length).to eq(2)
+      expect(response).to be_ok
+      expect(json_body.length).to eq(2)
+    end
+
+    context 'when there is no authorization header' do
+      it 'returns 403 (unauthorized)' do
+        get '/users'
+
+        expect(response).to be_unauthorized
+      end
+    end
   end
 
-  context 'when there is no authorization header' do
-    it 'returns 403 (unauthorized)' do
-      get '/users'
+  describe 'POST users' do
+    let(:email)    { 'test@estemity.com' }
+    let(:password) { 'password123' }
 
-      expect(response).to be_unauthorized
+    let(:params) do
+      { email: email, password: password }
+    end
+
+    it 'creates a user with email and password' do
+      expect { post '/users', params: params }.to change { User.count }.by(1)
+
+      expect(response).to be_created
+      expect(json_body.keys).to match_array(['id', 'email', 'created_at'])
+      expect(json_body['email']).to eq email
+    end
+
+    context 'when validation fails' do
+      let(:email) { 'foobar' }
+
+      it 'returns an error' do
+        expect { post '/users', params: params }.to_not change { User.count }
+
+        expect(response).to be_bad_request
+        expect(json_body['error']).to eq 'Email is invalid'
+      end
     end
   end
 end
